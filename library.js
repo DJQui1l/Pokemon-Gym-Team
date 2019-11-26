3a58634708923aa2cab5f73d1d041f9e897e9d46
Darian Jarvis <djarvis@nyc.yearup.org>
2019-11-26 13:01:10 -0500

    Made Trainer class with constructor this.name and this.team=[]

--- library.js
+++ library.js
@@ -1,34 +1,6 @@
-function start() {
-  let clicklightning = document.getElementById('lightning-bolt');
-  let clicktitle = document.getElementById('title');
-  let clickgrid = document.getElementById('title-grid');
-  let collapseableGrid = document.getElementById('collapseable-grid');
-
-  clicklightning.addEventListener("click", function() {
-
-    clicklightning.style.animation = "fade-out 1s ease-out";
-
-    setTimeout(() => {
-      clicklightning.style.display = "none";
-      clicktitle.style.display = "flex";
-      clicktitle.style.animation = "fade-in 1s ease-in, scale 4s ease-out";
-    }, 1004)
-
-    setTimeout(() => {
-      clicktitle.style.animation = "fade-out 1s ease-in, scale 4s ease-out";
-    }, 4000)
-
-    setTimeout(() => {
-      clicktitle.style.display = "none";
-      clickgrid.style.display = "none";
-      clicktitle.remove()
-      clickgrid.remove()
-
-      collapseableGrid.style.display = "flex"
-      collapseableGrid.style.animation = "fade-in 1s ease-in"
-    }, 5000)
-
-  })
+class Trainer {
+  constructor(name){
+    this.name
+    this.team = []
+  }
 }
-
-start()
