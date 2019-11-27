ff5989455f9a9e57af0f6c99edb68a5fcff12829
Tiffany Silverio <tsilverio@nyc.yearup.org>
2019-11-26 22:07:03 -0500

    Added functions to the images, now are clickable, need to make it pass.

--- library.js
+++ library.js
@@ -1,6 +1,8 @@
 function start() {
   let clicklightning = document.getElementById('lightning-bolt');
-  let clickletterD = document.getElementById('letterD')
+  let clicknadi = document.getElementById('nadira-logo');
+  let clicktiff = document.getElementById('tiffany-logo');
+  let clicksteph = document.getElementById('steph-logo');
   let clicktitle = document.getElementById('title');
   let clickgrid = document.getElementById('title-grid');
   let collapseableGrid = document.getElementById('collapseable-grid');
@@ -29,6 +31,68 @@ function start() {
       collapseableGrid.style.animation = "fade-in 1s ease-in"
     }, 5000)
 
+  })
+   clicknadi.addEventListener('click', function() {
+    clicknadi.style.animation = 'fade-out 1s ease-out';
+
+    setTimeout(() => {
+       clicknadi.style.display = 'none';
+       clicktitle.style.display = 'flex';
+       clicktitle.style.animation = 'fade-in 1s ease-in, scale 4s ease-out';
+    }, 1004)
+
+    setTimeout(() => {
+      clicktitle.style.animation = 'fade-out 1s ease-in, scale 4s ease-out';
+    }, 4000)
+
+    setTimeout(() => {
+      clicktitle.style.display = 'none';
+      clickgrid.style.display = 'none';
+      clicktitle.remove()
+      clickgrid.remove()
+
+      collapseableGrid.style.display = 'fade-out 1s ease-on, scale 4s ease-out';
+    }, 5000)
+  })
+
+  clicktiff.addEventListener('click', function() {
+    clicktiff.style.animation = 'fade-out 1s ease-out';
+
+    setTimeout(() => {
+      clicktiff.style.display = 'none';
+      clicktitle.style.display = 'flex';
+      clicktitle.style.animation = 'fade-in 1s ease-in, scale 4s ease-out';
+    }, 1004)
+
+    setTimeout(() => {
+      clicktitle.style.animation = 'fade-out 1s ease-in, scale 4s ease-out';
+    }, 4000)
+
+    setTimeout(() => {
+      clicktitle.style.display = 'none';
+      clickgrid.style.display ='none';
+      clicktitle.remove()
+       clickgrid.remove()
+
+       collapseableGrid.style.display = 'fade-out 1s ease-on, scale 4s ease-out';
+    }, 5000)
+  })
+
+  clicksteph.addEventListener('click', function() {
+    clicksteph.style.animation = 'fade-out 1s ease-out';
+
+    setTimeout(() => {
+      cliclsteph.style.animation = 'fade-out 1s ease-in, scale 4s ease-out';
+    }, 4000)
+
+    setTimeout(() => {
+      clicktitle.style.display = 'none';
+      clickgrid.style.display = 'none';
+      clicktitle.remove()
+      clickgrid.remove()
+
+      collapseableGrid.style.display = 'fade-out 1s ease-on, scale 4s ease-out'
+    }, 5000)
   })
 }
 
